c55633c0d18586f9651ef381b03368b3295af80d
catalinmoraru82 <catalinmoraru82@gmail.com>
2022-03-22 00:12:15 +0200

    update components
    Add more components and logic to components

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Text 1 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Text 2 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Text 3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ]
+  
+  export default FeedbackData
