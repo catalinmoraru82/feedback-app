eb1e01df9fe7443191c79729fa38d44eb4c0bf94
catalinmoraru82 <catalinmoraru82@gmail.com>
2022-03-23 20:47:48 +0200

    new component, icons,
    Add new component, icons package and logic

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,21 @@
-function FeedbackItem({ item }) {
+import { FaTimes } from 'react-icons/fa'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
+
+function FeedbackItem({ item, handleDelete }) {
     return (
-        <div className="card">
+        <Card>
             <div className="num-display">{item.rating}</div>
             <div className="text-display">{item.text}</div>
-        </div>
+            <button onClick={() => handleDelete(item.id)} className='close'>
+                <FaTimes color='purple' />
+            </button>
+        </Card>
     )
+}
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
