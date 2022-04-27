7aeeb0b5508946fc3cd5c806ac5be47716f4d044
catalinmoraru82 <catalinmoraru82@gmail.com>
2022-04-27 23:03:35 +0300

    Added new components
    Added new components like rating and made the submit function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { v4 as uuidv4} from 'uuid'
 import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    function addNewFeedback(newFeedback) {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
 
     function deleteFeedback(id) {
         if(window.confirm('Are you sure you want to delete?')) {
@@ -18,6 +25,7 @@ function App() {
         <>
             <Header/>
             <div className="container">
+                <FeedbackForm handleAdd = {addNewFeedback} />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
             </div>
